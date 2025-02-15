5f7d3edb79877810a82904c7d98d561686589edc
samandar <samandarerror@gmail.com>
2025-02-15 14:33:43 +0500

    add two numbers done

---
+++ src/easy/math/add_two_integers/Main.java
@@ -0,0 +1,10 @@
+package easy.math.add_two_integers;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 12, num2 = 5;
+        Solution solution = new Solution();
+        int sum = solution.sum(num1, num2);
+        System.out.println(sum);
+    }
+}
---
+++ src/easy/math/add_two_integers/Solution.java
@@ -0,0 +1,7 @@
+package easy.math.add_two_integers;
+
+public class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
